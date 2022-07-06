014936c2e0bf7a55d89ad3d7a7875035dfa17045
Teddy Xinyuan Chen <45612704+tddschn@users.noreply.github.com>
2022-07-06 15:45:24 +0800

    Bump version: 0.1.3 → 0.1.4

--- servicenow_test_runner_scheduler/__init__.py
+++ servicenow_test_runner_scheduler/__init__.py
@@ -1,3 +1,3 @@
-__version__ = '0.1.3'
+__version__ = '0.1.4'
 __app_name__ = 'servicenow-test-runner-scheduler'
 __description__ = 'ServiceNow - Start and Schedule Multiple ATF test runners in browser'
